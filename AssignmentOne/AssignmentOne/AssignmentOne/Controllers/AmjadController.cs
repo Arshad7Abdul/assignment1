b8c20fe95bc7bb0fa4f93bfea9a47cdda12c6256
amjadshaik4 <132248015+amjadshaik4@users.noreply.github.com>
2023-06-05 11:57:52 -0500

    pract

--- AssignmentOne/AssignmentOne/AssignmentOne/Controllers/AmjadController.cs
+++ AssignmentOne/AssignmentOne/AssignmentOne/Controllers/AmjadController.cs
@@ -1,6 +1,9 @@
 ﻿using AssignmentOne.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+
 
 namespace AssignmentOne.Controllers
 {
@@ -17,12 +20,55 @@ namespace AssignmentOne.Controllers
         [HttpPost]
         public ActionResult SubmitData1(Studentdata dataStudent)
         {
-            // Do something with the form data, for example:
-            // Save the student to a database, send an email notification, etc.
-            // ...
+
+            string connectionString = @"Data Source=(local);Initial Catalog=Amjad;User ID=opsuser;Password=Opt1mus;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Open the connection
+                connection.Open();
+
+                // Define the SQL query with parameters
+                string query = "INSERT INTO dbo.amjadDetails (fName, lName, myAdd, myEmail, myPhone, myGender, myTech, mySubmitDate, myTerms) VALUES (@value1, @value2, @value3, @value4, @value5, @value6, @value7, @value8, @value9)";
+
+                // Create a command object with the query and connection
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Set the parameter values
+                    command.Parameters.AddWithValue("@value1", dataStudent.fName);
+                    command.Parameters.AddWithValue("@value2", dataStudent.lName);
+                    command.Parameters.AddWithValue("@value3", dataStudent.myAdd);
+                    command.Parameters.AddWithValue("@value4", dataStudent.myEmail);
+                    command.Parameters.AddWithValue("@value5", dataStudent.myPhone);
+                    command.Parameters.AddWithValue("@value6", dataStudent.myGender);
+                    command.Parameters.AddWithValue("@value7", dataStudent.myTech);
+                    command.Parameters.AddWithValue("@value8", dataStudent.mySubmitTime);
+                    command.Parameters.AddWithValue("@value9", dataStudent.myTerms);
+                   
+
+                    // Execute the query
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    // Check if any rows were affected
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine("Data inserted successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data inserted.");
+                    }
+                }
+
+                // Close the connection
+                connection.Close();
+            }
+
+
+            
 
             // Return a response to the user, for example:
-            return Content($"Thank you, {dataStudent.fName} {dataStudent.lName} {dataStudent.myEmail}{dataStudent.myPhone}, for submitting your information.");
+            return Content($"Thank you, {dataStudent.fName} {dataStudent.lName}, for submitting your information.");
         }
     
 
--- AssignmentOne/AssignmentOne/AssignmentOne/Models/Studentdata.cs
+++ AssignmentOne/AssignmentOne/AssignmentOne/Models/Studentdata.cs
@@ -6,12 +6,12 @@
         public string lName { get; set; }
         public string myAdd { get; set; }
         public string myEmail { get; set; }
-        public int myPhone { get; set; }
+        public string myPhone { get; set; }
         public string myGender { get; set; }
         public string myTech { get; set; }
-        //public int mySubmitDate { get; set; }
-
+        
         public DateTime mySubmitTime { get; set; } = DateTime.Now;
+        public bool myTerms { get; set; }
 
 
         
